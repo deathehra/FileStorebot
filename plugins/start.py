313c44a6c6eec91e90b32a50ffebb38b88af82ea
deathehra <jarvidniies@gmail.com>
2025-12-03 23:24:17 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -225,9 +225,10 @@ async def start_command(client: Client, message: Message):
 
     else:
         reply_markup = InlineKeyboardMarkup([
-            [InlineKeyboardButton("CHANNELS", url="https://t.me/Movies8777")],
-            [InlineKeyboardButton("ABOUT", callback_data="about"),
-             InlineKeyboardButton("HELP", callback_data="help")]
+            [InlineKeyboardButton("мσνιє'ѕ", url="https://t.me/Movies8777"),
+             InlineKeyboardButton("нєηαтєє", url="https://t.me/+1epnsIzoCx43YTk1")]
+            [InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data="about"),
+             InlineKeyboardButton("ʜᴇʟᴘ", callback_data="help")]
         ])
         await message.reply_photo(
             photo=START_PIC,
@@ -290,7 +291,7 @@ async def not_joined(client: Client, message: Message):
                 except Exception as e:
                     print(f"Error with chat {chat_id}: {e}")
                     return await temp.edit(
-                        f"<b><i>! Eʀʀᴏʀ, Cᴏɴᴛᴀᴄᴛ ᴅᴇᴠᴇʟᴏᴘᴇʀ ᴛᴏ sᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇs @rohit_1888</i></b>\n"
+                        f"<b><i>! Eʀʀᴏʀ, Cᴏɴᴛᴀᴄᴛ ᴅᴇᴠᴇʟᴏᴘᴇʀ ᴛᴏ sᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇs @GoatHunterr</i></b>\n"
                         f"<blockquote expandable><b>Rᴇᴀsᴏɴ:</b> {e}</blockquote>"
                     )
 
@@ -318,7 +319,7 @@ async def not_joined(client: Client, message: Message):
     except Exception as e:
         print(f"Final Error: {e}")
         await temp.edit(
-            f"<b><i>! Eʀʀᴏʀ, Cᴏɴᴛᴀᴄᴛ ᴅᴇᴠᴇʟᴏᴘᴇʀ ᴛᴏ sᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇs @rohit_1888</i></b>\n"
+            f"<b><i>! Eʀʀᴏʀ, Cᴏɴᴛᴀᴄᴛ ᴅᴇᴠᴇʟᴏᴘᴇʀ ᴛᴏ sᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇs @GoatHunterr</i></b>\n"
             f"<blockquote expandable><b>Rᴇᴀsᴏɴ:</b> {e}</blockquote>"
         )
 
